c6c9cbdf87e6575f6a6139860b3f5f11b5bd65fc
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-21 20:45:59 +0200

    fix problem with longer user"s name, email

--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -17,24 +17,32 @@ interface IUserItemProps {
 }
 
 const UserItem: FC<IUserItemProps> = ({
-  user: { name, email, phone, position, photo = photoCover },
+  user: { name, email, phone, position, photo },
 }) => {
   return (
-    <li className="w-full p-5 bg-white rounded-[10px] flex items-center justify-center flex-col">
+    <li className="w-full p-5 bg-white rounded-[10px] flex items-center justify-center flex-col truncate">
       <Image
         width={70}
         height={70}
         alt={position}
-        src={photo}
+        src={photo || photoCover}
         className="rounded-full"
       />
-      <p className="my-5 text-base/[26px]">{name}</p>
+      <p
+        className="my-5 text-base/[26px] w-full truncate"
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content={name}
+        data-tooltip-place="bottom"
+      >
+        {name}
+      </p>
       <p>{position}</p>
       <a
         data-tooltip-id="my-tooltip"
         data-tooltip-content={email}
         data-tooltip-place="bottom"
         href={`mailto:${email}`}
+        className="w-full truncate"
       >
         {email}
       </a>
@@ -43,6 +51,7 @@ const UserItem: FC<IUserItemProps> = ({
         data-tooltip-content={phone}
         data-tooltip-place="bottom"
         href={`tel:${phone}`}
+        className="w-full truncate"
       >
         {phone}
       </a>
