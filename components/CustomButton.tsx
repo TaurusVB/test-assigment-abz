de34b29adfede030a95595c1b2b361a75a8ee865
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-21 20:17:45 +0200

    finish doing functionality users section with request to API

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,18 +3,21 @@
 import { FC } from "react";
 
 interface ICustomButtonProps {
-  disabled: boolean;
+  disabled?: boolean;
   type?: "submit" | "reset" | "button";
   text: string;
+  onClick?: () => void;
 }
 
 const CustomButton: FC<ICustomButtonProps> = ({
   disabled,
   type = "submit",
   text,
+  onClick,
 }) => {
   return (
     <button
+      onClick={onClick}
       disabled={disabled}
       type={type}
       className=" transition w-[120px] h-[34px] text-base/[26px] text-black/[87] flex items-center justify-center bg-yellow rounded-[80px] hover:bg-[#FFE302] mx-auto disabled:bg-[#B4B4B4] disabled:cursor-not-allowed"
